2ae8ae3e38393c4bdf4963b6a69431aa94451990
Peo Tuan <Peotuan.tav@gmail.com>
2023-04-23 14:52:29 +0700

    .add terms & conditions screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'valen';
+  title = "valen";
   initial: boolean = true;
+  isSubmitTerms = false;
 
   constructor() {}
 
   ngOnInit(): void {
+    const isSubmitInLocal = localStorage.getItem("isSubmitTerms");
+    this.isSubmitTerms = isSubmitInLocal === "YES" ? true : false;
     // setTimeout(() => {
     //   this.initial = false;
     //   this.router.navigateByUrl('login');
     // }, 3000);
+  }
+
+  public getSubmitTerm(event: boolean) {
+    this.isSubmitTerms = event;
   }
 }
